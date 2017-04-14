9b1eaf8eca9ccec67ab827e402f53a653ac03a14
Daniel Alzugaray <dalzuga@gmail.com>
2017-04-14 15:11:16 -0700

    Adding printf function file.

---
+++ printf.c
@@ -0,0 +1,19 @@
+#include <unistd.h> 		/* write(2) */
+#include <stdarg.h>		/* va_arg(3) */
+
+/**
+ * Allowed functions:
+ *
+ * write (man 2 write)
+ * malloc (man 3 malloc)
+ * free (man 3 free)
+ * va_start (man 3 va_start)
+ * va_end (man 3 va_end)
+ * va_copy (man 3 va_copy)
+ * va_arg (man 3 va_arg)
+ */
+
+int _printf(const char *format, ...)
+{
+	return (0);
+}
