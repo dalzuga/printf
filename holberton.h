7e6093c5412a1b95a6433398eacbbded9c0f26d5
Daniel Alzugaray <dalzuga@gmail.com>
2017-09-18 12:21:54 -0700

    Second prototype. {OK}

--- holberton.h
+++ holberton.h
@@ -1,3 +1,5 @@
 int _printf(const char *format, ...);
 void _puts(const char *s);
 void _putchar(char c);
+void print_number(int n);
+int count_digits(int n);
---
+++ print_d.c
@@ -0,0 +1,58 @@
+#include "holberton.h"
+
+void print_d(int d)
+{
+	print_number(d);
+}
+
+void print_number(int n)
+{
+	int aux1, i, count, x;	/* declaring variables */
+	if (n<0)
+		_putchar('-');	/* if negative, insert negative sign */
+
+	/* handles special case of extreme negative */
+	if (n==-2147483648)
+	{
+		_putchar(50);
+		n = -147483648;
+	}
+
+	if (n<0)
+		n=-n;		/* if negative, go into positive world */
+
+	count = count_digits(n);
+	x = 1;
+
+	for (i=1;i<count;i++)	/* make x big */
+		x=x*10;
+
+	_putchar((n/x)+48);
+
+	if (x>1)
+	{
+		aux1= n - ((int) n/x) * x; /* store magic formula */
+		/* if there's any zero's, don't miss them */
+		for (i=1;i<count_digits(n)-count_digits(aux1);i++)
+		{
+			_putchar(48);
+		}
+		print_number(n - ((int) n/x) * x); /* recursive protocol */
+	}
+}
+
+int count_digits(int n)
+{
+	int i=0, x=1;
+
+	if (n==0)
+		return 1;
+	else if (n>=1000000000) /* modify this to suit other than 32-bit */
+		return 10;
+	while (x<=n)
+	{
+		i++;
+		x=x*10;
+	}
+	return i;
+}
